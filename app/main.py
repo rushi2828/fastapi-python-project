c6c5cc8ef5ca152ed4e07f7f552623a0508d79c4
rushi2828 <rushi2828@gmail.com>
2025-03-06 00:39:25 +0530

    Added docker-compose.yml to the project

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Step 1: Configure the MySQL Database URL
-DATABASE_URL = "mysql+pymysql://root:Rushi*123@localhost:3306/pyton-project-db"
+# DATABASE_URL = "mysql+pymysql://root:Rushi*123@localhost:3306/pyton-project-db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Step 2: Initialize SQLAlchemy Engine and Session
 engine = create_engine(DATABASE_URL)
